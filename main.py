9a6132d8ce14b4c0c03223a25c758141988abca2
Paulo Eduardo <pe831365@gmail.com>
2024-02-09 01:00:58 -0300

    Feat: Bug Fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # código que agenda tarefas
 
 import subprocess # Eu estou utilizando este módulo para limpar o console
+import time
 
 tarefas = []
 
@@ -12,24 +13,38 @@ def listarTarefas():
     removerPrint()
     tamanho = len(tarefas)
     if tamanho == 0:
+        removerPrint()
         print('\nNão existem tarefas\n')
+        time.sleep(1)
         main()
-    for i, (tarefa) in enumerate(tarefas, 1):
-        print(f'{i}. {tarefa}') # VERSÃO JAVASCRIPT --> console.log(`${i}. ${tarefa} (Data: ${data})`)
-    print('')
+    else:
+        for i, (tarefa) in enumerate(tarefas, 1):
+            print(f'{i}. {tarefa}\n') # VERSÃO JAVASCRIPT --> console.log(`${i}. ${tarefa} (Data: ${data})`)
+        input('Pressione \'Enter\' para voltar para o menu.')
     main()
 
 def adicionarTarefa():
     removerPrint()
     tarefa = input('Nome da tarefa: ')
     tarefas.append((tarefa))
+
+    if (tarefa == ''):
+        removerPrint()
+        print('Erro! Digite alguma coisa!')
+        time.sleep(1)
+        adicionarTarefa()
+
     choice = int(input('tarefa adicionada com sucesso!\nDeseja adicionar mais uma tarefa?\n1 - sim | 2 - não"\n'))
     if choice == 1:
         adicionarTarefa()
-    if choice == 2:
+    elif choice == 2:
         main()
     else:
+        removerPrint()
         print('Erro')
+        time.sleep(1)
+        main()
+        
     
 # Função para remover um item da lista
 def teste():
@@ -37,10 +52,10 @@ def teste():
     listarTarefas2()
     indice = int(input('\nDigite qual tarefa você deseja remover: '))
     del tarefas[indice]
-    choice = int(input('Deseja remover outra tarefa?\n1 - Sim | 2 - Não'))
-    if choice == 1:
+    choice = input('Deseja remover outra tarefa?\n1 - Sim | 2 - Não')
+    if choice == '1':
         removerTarefa()
-    elif choice == 2:
+    elif choice == '2':
         main()
     else:
         print('erro')
@@ -50,9 +65,10 @@ def removerTarefa():
     listarTarefas2()
     
     if len(tarefas) == 0:
+        removerPrint()
         print("A lista de tarefas está vazia.")
+        time.sleep(2)
         main()
-        return
     
     try:
         indice = int(input('\nDigite qual tarefa você deseja remover: ')) - 1  # Subtrai 1 do índice para corresponder ao índice da lista (que começa em 0)
@@ -81,13 +97,19 @@ def removerPrint():
 
 # Função principal que funciona como uma espécie de "Menu"
 def main():
-    valor = int(input('Agendador de tarefas\nSelecione a opção:\n1 - listar tarefas | 2 - adicionar tarefa | 3 - remover tarefa\n'))
+    removerPrint()
+    valor = input('Agendador de tarefas\nSelecione a opção:\n1 - listar tarefas | 2 - adicionar tarefa | 3 - remover tarefa\n')
     
-    if valor == 1:
+    if valor == '1':
         listarTarefas()
-    if valor == 2:
+    elif valor == '2':
         adicionarTarefa()
-    if valor == 3:
+    elif valor == '3':
         removerTarefa()
+    else:
+        removerPrint()
+        print('Erro! Coloque algum valor')
+        time.sleep(2)
+        main()
 
 main()
